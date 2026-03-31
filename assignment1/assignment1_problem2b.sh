5f637389d0fe81a3def866b77671f3b54aba20bc
LukeQian <EduardoQian@gmail.com>
2026-03-31 21:35:57 +0200

    fix data problem

--- assignment1/assignment1_problem2b.sh
+++ assignment1/assignment1_problem2b.sh
@@ -12,7 +12,7 @@ cd "$JOB_DIR"
 
 export JOB_DIR
 
-apptainer exec --bind "$JOB_DIR:$JOB_DIR" "$JOB_DIR/assignment1.sif" bash -s <<'EOF'
+apptainer exec --bind "$JOB_DIR:$JOB_DIR" --bind /data:/data "$JOB_DIR/assignment1.sif" bash -s <<'EOF'
 set -euo pipefail
 
 dataset="/data/courses/2026_dat471_dit066/datasets/bike_sharing_hourly.csv"
