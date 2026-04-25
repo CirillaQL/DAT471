22befb41a11e7085d9c502e2dee6b97279eefab7
LukeQian <EduardoQian@gmail.com>
2026-04-25 20:27:57 +0200

    add problem2c part

--- assignment2/assignment2_problem2c.py
+++ assignment2/assignment2_problem2c.py
@@ -64,7 +64,8 @@ def get_top10(counts):
     Return value:
     A list of (count,word) pairs (int,str)
     """
-    raise NotImplementedError
+    sorted_counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+    return [(v, k) for k, v in list(sorted_counts.items())[:10]]
 
 
 
@@ -99,7 +100,10 @@ def compute_checksum(counts):
     Return value:
     The checksum (int)
     """
-    raise NotImplementedError
+    checksum = 0
+    for (k,v) in counts.items():
+        checksum += len(k) * v
+    return checksum
 
 
 if __name__ == '__main__':
@@ -125,13 +129,40 @@ if __name__ == '__main__':
         sys.stderr.write(f'{sys.argv[0]}: ERROR: Batch size must be positive (got {batch_size})!\n')
         quit(1)
 
+    time_total_start = time.time()
+    time_measurement_a_start = time.time()
     files = [get_file(fn) for fn in get_filenames(path)]
+    time_measurement_a_end = time.time()
+    print(f'Time for reading files: {time_measurement_a_end - time_measurement_a_start:.2f} seconds')
 
+    time_measurement_b_start = time.time()
     file_counts = list()
     for file in files:
         file_counts.append(count_words_in_file(file))
+    time_measurement_b_end = time.time()
+    print(f'Time for counting words in files: {time_measurement_b_end - time_measurement_b_start:.2f} seconds')
 
+    time_measurement_c_start = time.time()
     global_counts = dict()
     for counts in file_counts:
         merge_counts(global_counts,counts)
+    time_measurement_c_end = time.time()
+    print(f'Time for merging counts: {time_measurement_c_end - time_measurement_c_start:.2f} seconds')
+
+    time_measurement_d_start = time.time()
+    top10 = get_top10(global_counts)
+    time_measurement_d_end = time.time()
+    print(f'Time for getting top10: {time_measurement_d_end - time_measurement_d_start:.2f} seconds')   
+
+    time_measurement_e_start = time.time()
+    checksum = compute_checksum(global_counts)
+    time_measurement_e_end = time.time()
+    print(f'Time for computing checksum: {time_measurement_e_end - time_measurement_e_start:.2f} seconds')
     
+    print(f'Checksum: {checksum}')
+    print('Top 10:')
+    for (count, word) in top10:
+        print(f'{word}: {count}')   
+
+    time_total_end = time.time()
+    print(f'Total time: {time_total_end - time_total_start:.2f} seconds')
