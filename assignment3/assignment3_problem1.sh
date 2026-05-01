4eef0313f92e2db901ec5cc33ab7431e37678ac0
LukeQian <EduardoQian@gmail.com>
2026-05-01 11:43:24 +0200

    change python file path

--- assignment3/assignment3_problem1.sh
+++ assignment3/assignment3_problem1.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 
 CONTAINER="/data/courses/2026_dat471_dit066/containers/assignment3.sif"
 DATASET_PATH="/data/courses/2026_dat471_dit066/datasets/sc2/planets.csv"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="/data/users/luqia/DAT471/assignment3"
 SCRIPT_PATH="${SCRIPT_DIR}/assignment3_problem1.py"
 NUM_CORES="${SLURM_CPUS_PER_TASK:-1}"
 
